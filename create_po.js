4f2279bac96cb6db7d9b33d24d23c8c3e4cc5f2b
sankar <sankar@zageno.com>
2019-10-07 11:54:27 +0200

    WIP on PO update

--- create_po.js
+++ create_po.js
@@ -6,7 +6,8 @@ function GET(datain) {
     return JSON.stringify(nlapiLoadRecord(datain.recordtype, datain.id));
 }
 
-function update_inventory_item(record, item) {
+function updateFieldValues(record, item) {
+  // Pass empty list in exclusionList if there is nothing to exclude
   for (var fieldname in item)
   {
     if (item.hasOwnProperty(fieldname))
@@ -18,64 +19,67 @@ function update_inventory_item(record, item) {
       }
     }
   }
-  var recordId = nlapiSubmitRecord(record);
-  nlapiLogExecution('DEBUG','id='+recordId);
-  return nlapiLoadRecord(inventoryitem, recordId);
+  return nlapiSubmitRecord(record);
+}
+
+function search(type, name, id) {
+  var filters = new Array();
+  filters[0] = new nlobjSearchFilter(name, null, 'contains', id, null);
+  return nlapiSearchRecord(type, null, filters, null);
 }
 
 function get_item(item) {
-  var filters = new Array();
   var itemRecord;
-  filters[0] = new nlobjSearchFilter('itemid', null, 'contains', item.itemid, null);
-  var searchresults = nlapiSearchRecord(inventoryitem, null, filters, null);
-
+  var searchresults = search(inventoryitem, 'itemid', item.itemid);
   if (searchresults !== null && searchresults.length==1) {
     itemRecord = nlapiLoadRecord(inventoryitem, searchresults[0].getId());
   }
   else {
     itemRecord = nlapiCreateRecord(inventoryitem);
   }
-  return update_inventory_item(itemRecord, item);
+  var recordId = updateFieldValues(itemRecord, item);
+  return nlapiLoadRecord(inventoryitem, recordId);
 }
 
 function addItem(record, item) {
-  var lineItem = get_item(item);
+  var item = get_item(item);
   record.selectNewLineItem('item');
-  record.setCurrentLineItemValue('item','item', lineItem.getId());
-  //record.setCurrentLineItemValue('item', 'price', item.price);
-  //record.setCurrentLineItemValue('item','isbillable','T');
+  record.setCurrentLineItemValue('item','item', item.getId());
+  //record.setCurrentLineItemValue('item', 'location', 2);
   record.commitLineItem('item');
 }
 
+function removeLineItems(record) {
+  for (var j=record.getLineItemCount('item'); j>=1; j--)
+  {
+    record.removeLineItem('item', j);
+  }
+}
 
-function POST(datain) {
-  var record = nlapiCreateRecord(purchaseorder);
+function updatePO(record, datain) {
   var item;
   for (var index in datain.items) {
     item = datain.items[index];
     addItem(record, item);
   }
+  delete(datain.items);
+  var recordId = updateFieldValues(record, datain);
+  return nlapiLoadRecord(purchaseorder, recordId);
+}
 
-  for (var fieldname in datain)
-  {
-    if (datain.hasOwnProperty(fieldname))
-      {
-        if (fieldname != 'recordtype' && fieldname != 'id' && fieldname != 'items')
-        {
-          var value = datain[fieldname];
-          if (value && typeof value != 'object')
-            {
-              record.setFieldValue(fieldname, value);
-            }
-         }
-     }
-  }
-  var recordId = nlapiSubmitRecord(record);
-  nlapiLogExecution('DEBUG','id='+recordId);
+function POST(datain) {
+  var record = nlapiCreateRecord(purchaseorder);
+  return updatePO(record, datain);
+}
 
-  return nlapiLoadRecord(purchaseorder,recordId);
+function PUT(datain) {
+  var record = nlapiLoadRecord(purchaseorder, datain.id);
+  removeLineItems(record);
+  nlapiDeleteRecord(purchaseorder, datain.id);
+  delete(datain.id);
+  return updatePO(nlapiCreateRecord(purchaseorder), datain);
 }
 
 function DELETE(data) {
-  nlapiDeleteRecord(purchaseorder, data.id);
+  return "No Dont Delete";
 }
